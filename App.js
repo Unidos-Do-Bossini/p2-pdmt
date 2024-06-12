3c89dd7bc318d099f773862acc2e557cbdafcd20
João Souza <joaosvieira04@gmail.com>
2024-06-12 12:15:00 -0300

    feat(p2-pdmt): Adiciona lógica de busca e exibição de fotos

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, Text, Pressable, FlatList, View, Image } from 'react-native';
+import { getGato } from './services/GatitosService.js';
 
 export default function App() {
+  const [fotos, setFotos] = useState([])
+
+  const getFotos = async () => {
+    const request = await getGato()
+    setFotos(request.slice(0, 5))
+  }
+
+  const renderItem = ({item}) => (
+    <Image
+      source={{uri: item.url}}
+      style={{width: '30%', height: 200, marginVertical: 10}}
+    />
+  ) 
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1, padding: 20}}>
+      <Pressable
+        onPress={getFotos}
+        style={({pressed}) => [{
+          backgroundColor: pressed ? '#FF0000' : '#FFFF00',
+          padding: 20,
+          alignItems: 'center',
+          marginBottom: 10
+        }]
+      }
+          >
+        <Text>CLICA AQUI</Text>
+      </Pressable>
+      <FlatList
+      data={fotos}
+      renderItem={renderItem}
+      keyExtractor={(item, index) => index.toString()}
+      ></FlatList>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
